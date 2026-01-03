855bd38a60f142cbcc6f6e3061f063a7be003b2c
BR05 <21fortnitegod@gmail.com>
2026-01-03 18:15:57 +0000

    .lua

--- .lua
+++ .lua
@@ -1,7 +1,7 @@
 local urls = {
     "https://raw.githubusercontent.com/BR05Lua/SOS/refs/heads/main/BR05TagSystem.lua",
     "https://raw.githubusercontent.com/BR05Lua/SOS/refs/heads/main/SOSMenu.lua",
-    "https://raw.githubusercontent.com/user/script3.lua"
+    "https://raw.githubusercontent.com/BR05Lua/SOS/refs/heads/main/Security/BR05"
 }
 
 for _, url in ipairs(urls) do
